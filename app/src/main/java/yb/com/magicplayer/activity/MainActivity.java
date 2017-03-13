068f30d61660708ba63b1ec5646a1a16727882d4
yuanbin <2460666162@qq.com>
2017-03-13 19:51:28 +0800

    welcome init

--- app/src/main/java/yb/com/magicplayer/activity/MainActivity.java
+++ app/src/main/java/yb/com/magicplayer/activity/MainActivity.java
@@ -2,26 +2,15 @@ package yb.com.magicplayer.activity;
 
 
 import android.content.Intent;
-import android.graphics.Paint;
-import android.graphics.Path;
-import android.util.Log;
 import android.view.View;
-
-import java.io.File;
-
+import android.widget.TextView;
 import yb.com.magicplayer.R;
 import yb.com.magicplayer.service.MusicPlayService;
 import yb.com.magicplayer.utils.ActivityUtil;
-import yb.com.magicplayer.utils.FileUtil;
-import yb.com.magicplayer.utils.GlobalVariables;
-import yb.com.magicplayer.view.SvgView;
-import yb.com.magicplayer.view.Utils.SvgUtils;
-import yb.com.text2svg.entity.ResultData;
-import yb.com.text2svg.util.Text2SvgUtils;
+
 
 public class MainActivity extends BaseActivity {
-    private SvgView sv;
-
+    private TextView tv;
     @Override
     protected int getLayoutId() {
         return R.layout.activity_main;
@@ -29,21 +18,7 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-        sv = (SvgView) findViewById(R.id.svg);
-        ResultData data = null;
-        try {
-            data = Text2SvgUtils.toConvert(FileUtil.getOutDirPrivate(this, GlobalVariables.FONT_PATH).getAbsolutePath() + "/" + GlobalVariables.FONT_NAME, "B");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try {
-            SvgUtils utils = new SvgUtils(new Paint());
-            utils.load(this, data.getCharGlyphList().get(0).getD());
-            sv.setSvgPaths(utils.getPathsForViewport(512, 512));
-            sv.getPathAnimator().duration(5000).start();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        tv = (TextView) findViewById(R.id.id_local);
     }
 
     @Override
--- app/src/main/java/yb/com/magicplayer/activity/WelcomeActivity.java
+++ app/src/main/java/yb/com/magicplayer/activity/WelcomeActivity.java
@@ -1,20 +1,29 @@
 package yb.com.magicplayer.activity;
 
-import android.graphics.drawable.VectorDrawable;
-import android.net.Uri;
-import android.util.Log;
-
-import java.io.File;
-import java.io.IOException;
+import android.animation.Animator;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import yb.com.magicplayer.R;
 import yb.com.magicplayer.utils.ActivityUtil;
-import yb.com.magicplayer.utils.FileUtil;
 import yb.com.magicplayer.utils.GlobalVariables;
 import yb.com.magicplayer.utils.MediaUtils;
-import yb.com.text2svg.util.Text2SvgUtils;
+import yb.com.magicplayer.view.shimmer.Shimmer;
+import yb.com.magicplayer.view.shimmer.ShimmerTextView;
 
-public class WelcomeActivity extends BaseActivity {
+public class WelcomeActivity extends BaseActivity implements Animator.AnimatorListener {
+    private ShimmerTextView mSTv;
+    private Shimmer shimmer;
+    private boolean isPrepared;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     protected int getLayoutId() {
@@ -23,23 +32,32 @@ public class WelcomeActivity extends BaseActivity {
 
     @Override
     protected void initView() {
+        mSTv = (ShimmerTextView) findViewById(R.id.id_welcome_content);
     }
 
     @Override
     protected void initData() {
+        startAnim();
+        isPrepared = false;
         new Thread(new Runnable() {
             @Override
             public void run() {
                 prepareThings();
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        ActivityUtil.startActivity(getContext(), MainActivity.class);
-                        finish();
-                    }
-                });
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                isPrepared = true;
             }
         }).start();
+    }
+
+    private void startAnim() {
+        shimmer = new Shimmer();
+        shimmer.setAnimatorListener(this);
+        shimmer.setRepeatCount(1);
+        shimmer.start(mSTv);
     }
 
     private void prepareThings() {
@@ -49,17 +67,17 @@ public class WelcomeActivity extends BaseActivity {
 
     private void chackFile() {
         //检查font文件是否写入SD卡中
-        File file = FileUtil.getOutDirPrivate(getContext(), GlobalVariables.FONT_PATH);
-        if (file != null) {
-            file = new File(file.getAbsolutePath() + "/" + GlobalVariables.FONT_NAME);
-            if (file != null && !file.exists()) {
-                try {
-                    FileUtil.writeFile(WelcomeActivity.this.getAssets().open("slim.ttf"), file.getAbsolutePath());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+//        File file = FileUtil.getOutDirPrivate(getContext(), GlobalVariables.FONT_PATH);
+//        if (file != null) {
+//            file = new File(file.getAbsolutePath() + "/" + GlobalVariables.FONT_NAME);
+//            if (file != null && !file.exists()) {
+//                try {
+//                    FileUtil.writeFile(WelcomeActivity.this.getAssets().open("slim.ttf"), file.getAbsolutePath());
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
     }
 
     @Override
@@ -67,4 +85,29 @@ public class WelcomeActivity extends BaseActivity {
 
     }
 
+    @Override
+    public void onAnimationStart(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animation) {
+        if (isPrepared) {
+            shimmer.cancel();
+            //ActivityUtil.startActivity(getContext(), MainActivity.class);
+          //  finish();
+        } else {
+            startAnim();
+        }
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animation) {
+
+    }
 }
